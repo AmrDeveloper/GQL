e321bbd17df2a9e719707ad0bd5599be755ce7ea
AmrDeveloper <amr96@programmer.net>
2023-06-02 20:38:54 +0200

    Improve the structure of GQL parser

--- src/parser.rs
+++ src/parser.rs
@@ -11,78 +11,100 @@ pub fn parse_gql(tokens: Vec<Token>) -> Result<Vec<Box<dyn Statement>>, String> 
         let token = &tokens[position];
         match &token.kind {
             TokenKind::Select => {
-                position += 1;
-                let mut fields: Vec<String> = Vec::new();
-                if position >= len {
-                    return Err("Expect * or fields names after select keyword".to_owned());
+                let parse_result = parse_select_statement(&tokens, &mut position);
+                if parse_result.is_err() {
+                    return Err(parse_result.err().unwrap());
                 }
-
-                if tokens[position].kind != TokenKind::Star {
-                    while position < len {
-                        fields.push(tokens[position].literal.to_string());
-                        position += 1;
-                        if tokens[position].kind == TokenKind::Comma {
-                            position += 1;
-                        } else {
-                            break;
-                        }
-                    }
-                } else {
-                    position += 1;
-                }
-
-                if tokens[position].kind != TokenKind::From {
-                    return Err("Expect `from` keyword after attributes".to_owned());
-                }
-
-                position += 1;
-                let table_name = &tokens[position].literal;
-                position += 1;
-
-                let statement = SelectStatement {
-                    table_name: table_name.to_string(),
-                    fields,
-                };
-
-                statements.push(Box::new(statement));
-                continue;
+                statements.push(parse_result.ok().unwrap());
             }
             TokenKind::Where => {}
             TokenKind::Limit => {
-                position += 1;
-
-                if position >= len || tokens[position].kind != TokenKind::Number {
-                    return Err("Expect number after `limit` keyword".to_owned());
+                let parse_result = parse_limit_statement(&tokens, &mut position);
+                if parse_result.is_err() {
+                    return Err(parse_result.err().unwrap());
                 }
-
-                let count_str = tokens[position].literal.to_string();
-                let count: usize = count_str.parse().unwrap();
-
-                position += 1;
-
-                let limit_statement = LimitStatement { count };
-                statements.push(Box::new(limit_statement));
-                continue;
+                statements.push(parse_result.ok().unwrap());
             }
             TokenKind::Offset => {
-                position += 1;
-
-                if position >= len || tokens[position].kind != TokenKind::Number {
-                    return Err("Expect number after `offset` keyword".to_owned());
+                let parse_result = parse_offset_statement(&tokens, &mut position);
+                if parse_result.is_err() {
+                    return Err(parse_result.err().unwrap());
                 }
-
-                let count_str = tokens[position].literal.to_string();
-                let count: usize = count_str.parse().unwrap();
-
-                position += 1;
-
-                let offset_statement = OffsetStatement { count };
-                statements.push(Box::new(offset_statement));
-                continue;
+                statements.push(parse_result.ok().unwrap());
             }
-            _ => return Err("".to_owned()),
+            _ => return Err("Invalid statement".to_owned()),
         }
     }
 
     return Ok(statements);
+}
+
+fn parse_select_statement(
+    tokens: &Vec<Token>,
+    position: &mut usize,
+) -> Result<Box<dyn Statement>, String> {
+    *position += 1;
+    let mut fields: Vec<String> = Vec::new();
+    if *position >= tokens.len() {
+        return Err("Expect * or fields names after select keyword".to_owned());
+    }
+
+    if tokens[*position].kind != TokenKind::Star {
+        while *position < tokens.len() {
+            fields.push(tokens[*position].literal.to_string());
+            *position += 1;
+            if tokens[*position].kind == TokenKind::Comma {
+                *position += 1;
+            } else {
+                break;
+            }
+        }
+    } else {
+        *position += 1;
+    }
+
+    if tokens[*position].kind != TokenKind::From {
+        return Err("Expect `from` keyword after attributes".to_owned());
+    }
+
+    *position += 1;
+    let table_name = &tokens[*position].literal;
+    *position += 1;
+
+    let statement = SelectStatement {
+        table_name: table_name.to_string(),
+        fields,
+    };
+
+    return Ok(Box::new(statement));
+}
+
+fn parse_limit_statement(
+    tokens: &Vec<Token>,
+    position: &mut usize,
+) -> Result<Box<dyn Statement>, String> {
+    *position += 1;
+    if *position >= tokens.len() || tokens[*position].kind != TokenKind::Number {
+        return Err("Expect number after `limit` keyword".to_owned());
+    }
+
+    let count_str = tokens[*position].literal.to_string();
+    let count: usize = count_str.parse().unwrap();
+    *position += 1;
+    return Ok(Box::new(LimitStatement { count }));
+}
+
+fn parse_offset_statement(
+    tokens: &Vec<Token>,
+    position: &mut usize,
+) -> Result<Box<dyn Statement>, String> {
+    *position += 1;
+    if *position >= tokens.len() || tokens[*position].kind != TokenKind::Number {
+        return Err("Expect number after `offset` keyword".to_owned());
+    }
+
+    let count_str = tokens[*position].literal.to_string();
+    let count: usize = count_str.parse().unwrap();
+    *position += 1;
+    return Ok(Box::new(OffsetStatement { count }));
 }
