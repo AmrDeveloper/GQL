a4b190a35484942b7a689aeecdb780cbb0195fdc
AmrDeveloper <amr96@programmer.net>
2023-06-03 13:49:11 +0200

    Improve error message format

--- src/diagnostic.rs
+++ src/diagnostic.rs
@@ -1,11 +1,14 @@
 use crate::tokenizer::Location;
 
+const PORPOT_LENGTH: usize = 6;
+
 pub struct GQLError {
     pub message: String,
     pub location: Location,
 }
+
 pub fn report_gql_error(error: GQLError) {
-    println!("{}^", "-".repeat(6 + error.location.start));
+    println!("{}^", "-".repeat(PORPOT_LENGTH + error.location.start));
     println!(
         "Error({}:{}) -> {}",
         error.location.start, error.location.end, error.message
