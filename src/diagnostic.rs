4c5913a372106d1fa9a37dfaa230d646a282ba2b
AmrDeveloper <amr96@programmer.net>
2023-06-03 13:37:07 +0200

    Improve diagnostic error message format

--- src/diagnostic.rs
+++ src/diagnostic.rs
@@ -4,8 +4,8 @@ pub struct GQLError {
     pub message: String,
     pub location: Location,
 }
-
 pub fn report_gql_error(error: GQLError) {
+    println!("{}^", "-".repeat(6 + error.location.start));
     println!(
         "Error({}:{}) -> {}",
         error.location.start, error.location.end, error.message
