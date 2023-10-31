22d5dd61feba549f640bc5d1a737349dde06457a
AmrDeveloper <amr96@programmer.net>
2023-10-31 16:57:54 +0200

    feat: Migrate from Prettytables-rs to comfy-table for render tables

--- crates/gitql-cli/src/render.rs
+++ crates/gitql-cli/src/render.rs
@@ -1,5 +1,4 @@
 use gitql_ast::object::GQLObject;
-use prettytable::{Cell, Row, Table};
 
 pub fn render_objects(groups: &Vec<Vec<GQLObject>>, hidden_selections: &Vec<String>) {
     if groups.is_empty() || groups[0].is_empty() {
@@ -13,33 +12,34 @@ pub fn render_objects(groups: &Vec<Vec<GQLObject>>, hidden_selections: &Vec<Stri
         .map(|k| k.as_ref())
         .collect();
 
-    let mut table = Table::new();
-    let mut table_titles: Vec<Cell> = Vec::new();
+    let mut table = comfy_table::Table::new();
 
+    // Setup table style
+    table.load_preset(comfy_table::presets::UTF8_FULL);
+    table.apply_modifier(comfy_table::modifiers::UTF8_ROUND_CORNERS);
+    table.set_content_arrangement(comfy_table::ContentArrangement::Dynamic);
+
+    // Setup table headers
+    let header_color = comfy_table::Color::Green;
+    let mut table_headers = vec![];
     for key in &titles {
-        table_titles.push(Cell::new(key));
+        table_headers.push(comfy_table::Cell::new(key).fg(header_color));
     }
+    table.set_header(table_headers);
 
-    table.add_row(Row::new(table_titles));
-
-    let table_field_max_len = 40;
+    // Push table rows
     for group in groups {
         for object in group {
-            let mut table_row = Row::new(Vec::new());
+            let mut table_row: Vec<comfy_table::Cell> = vec![];
             for key in &titles {
                 let value = &object.attributes.get(&key as &str).clone().unwrap();
                 let value_literal = value.literal();
-                if value_literal.len() > table_field_max_len {
-                    let wrapped = textwrap::wrap(value_literal.as_str(), table_field_max_len);
-                    let formatted = wrapped.join("\n");
-                    table_row.add_cell(Cell::new(&formatted));
-                } else {
-                    table_row.add_cell(Cell::new(value_literal.as_str()));
-                }
+                table_row.push(comfy_table::Cell::new(value_literal.as_str()));
             }
             table.add_row(table_row);
         }
     }
 
-    table.printstd();
+    // Print table in Stdout
+    println!("{table}");
 }
