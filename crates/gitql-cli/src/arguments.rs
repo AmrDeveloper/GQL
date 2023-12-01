3b7e20843284451d49c978d2193630f9ab40dd48
AmrDeveloper <amr96@programmer.net>
2023-12-01 11:36:06 +0200

    feat: Revamp cli parser and allow one query mode

--- crates/gitql-cli/src/arguments.rs
+++ crates/gitql-cli/src/arguments.rs
@@ -1,36 +1,230 @@
-use clap::Parser;
-
-fn valid_page_size(input: &str) -> Result<usize, String> {
-    let page_size_result = input.parse::<usize>();
-    if page_size_result.is_ok() {
-        let page_size = page_size_result.ok().unwrap();
-        return Ok(page_size);
-    }
-    Err("Invalid page size".to_string())
-}
-
-/// GitQL is a SQL like query language to run on local repositories
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+/// Arguments for GitQL
+#[derive(Debug, PartialEq)]
 pub struct Arguments {
-    /// Path for local repositories to run query on
-    #[arg(short, long, num_args = 1..)]
-    #[structopt(required = true)]
     pub repos: Vec<String>,
-
-    /// Show analysis for front end and engine
-    #[clap(short, long, action)]
     pub analysis: bool,
-
-    /// Enable Pagination
-    #[clap(short, long, action, default_value = "false")]
     pub pagination: bool,
-
-    /// Set page size for Pagination
-    #[clap(short, long, action, default_value = "10", value_parser=valid_page_size)]
-    pub size_per_page: usize,
+    pub page_size: usize,
 }
 
-pub fn parse_arguments() -> Arguments {
-    Arguments::parse()
+/// Create a new instance of Arguments with the default settings
+impl Arguments {
+    fn new() -> Arguments {
+        Arguments {
+            repos: vec![],
+            analysis: false,
+            pagination: false,
+            page_size: 10,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Command {
+    ReplMode(Arguments),
+    QueryMode(String, Arguments),
+    Help,
+    Version,
+    Error(String),
+}
+
+pub fn parse_arguments(args: &Vec<String>) -> Command {
+    let args_len = args.len();
+
+    if args.iter().any(|i| i == "--help" || i == "-h") {
+        return Command::Help;
+    }
+
+    if args.iter().any(|i| i == "--version" || i == "-v") {
+        return Command::Version;
+    }
+
+    let mut optional_query: Option<String> = None;
+    let mut arguments = Arguments::new();
+
+    let mut arg_index = 1;
+    loop {
+        if arg_index >= args_len {
+            break;
+        }
+
+        let arg = &args[arg_index];
+
+        if !arg.starts_with('-') {
+            return Command::Error(format!("Unkown argument {}", arg));
+        }
+
+        match arg.as_ref() {
+            "--repos" | "-r" => {
+                arg_index += 1;
+                if arg_index >= args_len {
+                    let message = format!("Argument {} must be followed by one or more path", arg);
+                    return Command::Error(message);
+                }
+
+                loop {
+                    if arg_index >= args_len {
+                        break;
+                    }
+
+                    let repo = &args[arg_index];
+                    if !repo.starts_with('-') {
+                        arguments.repos.push(repo.to_string());
+                        arg_index += 1;
+                        continue;
+                    }
+
+                    break;
+                }
+            }
+            "--query" | "-q" => {
+                arg_index += 1;
+                if arg_index >= args_len {
+                    let message = format!("Argument {} must be followed by the query", arg);
+                    return Command::Error(message);
+                }
+
+                optional_query = Some(args[arg_index].to_string());
+                arg_index += 1;
+            }
+            "--analysis" | "-a" => {
+                arguments.analysis = true;
+                arg_index += 1;
+            }
+            "--pagination" | "-p" => {
+                arguments.pagination = true;
+                arg_index += 1;
+            }
+            "--pagesize" | "-ps" => {
+                arg_index += 1;
+                if arg_index >= args_len {
+                    let message = format!("Argument {} must be followed by the page size", arg);
+                    return Command::Error(message);
+                }
+
+                let page_size_result = args[arg_index].parse::<usize>();
+                if page_size_result.is_err() {
+                    return Command::Error("Invalid page size".to_string());
+                }
+
+                let page_size = page_size_result.ok().unwrap();
+                arguments.page_size = page_size;
+                arg_index += 1;
+            }
+            _ => return Command::Error(format!("Unkown command {}", arg)),
+        }
+    }
+
+    // Add the current directory if no repository is passed
+    if arguments.repos.is_empty() {
+        let current_dir = std::env::current_dir();
+        if current_dir.is_ok() {
+            arguments.repos.push(
+                current_dir
+                    .ok()
+                    .unwrap()
+                    .as_os_str()
+                    .to_str()
+                    .unwrap_or(".")
+                    .to_string(),
+            );
+        } else {
+            return Command::Error("Missing repositories paths".to_string());
+        }
+    }
+
+    if let Some(query) = optional_query {
+        Command::QueryMode(query, arguments)
+    } else {
+        Command::ReplMode(arguments)
+    }
+}
+
+pub fn print_help_list() {
+    println!("GitQL is a SQL like query language to run on local repositories");
+    println!();
+    println!("Usage: gitql.exe [OPTIONS]");
+    println!();
+    println!("Options:");
+    println!("-r,  --repos <REPOS>        Path for local repositories to run query on");
+    println!("-q,  --query <GQL Query>    GitQL query to run on selected repositories");
+    println!("-p,  --pagination           Enable print result with pagninations");
+    println!("-ps, --pagesize             Set pagination page size [default: 10]");
+    println!("-a,  --analysis             Print Query analysis");
+    println!("-h,  --help                 Print GitQL help");
+    println!("-v,  --version              Print GitQL Current Version");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_arguments() {
+        let arguments = vec!["gitql".to_string()];
+        let command = parse_arguments(&arguments);
+        assert!(matches!(command, Command::ReplMode { .. }));
+    }
+
+    #[test]
+    fn test_repl_arguments() {
+        let arguments = vec!["gitql".to_string(), "--repos".to_string(), ".".to_string()];
+        let command = parse_arguments(&arguments);
+        assert!(matches!(command, Command::ReplMode { .. }));
+    }
+
+    #[test]
+    fn test_query_arguments() {
+        let arguments = vec![
+            "gitql".to_string(),
+            "-q".to_string(),
+            "Select * from table".to_string(),
+        ];
+        let command = parse_arguments(&arguments);
+        assert!(matches!(command, Command::QueryMode { .. }));
+    }
+
+    #[test]
+    fn test_arguments_with_help() {
+        let arguments = vec![
+            "gitql".to_string(),
+            "dummy".to_string(),
+            "--help".to_string(),
+        ];
+        let command = parse_arguments(&arguments);
+        assert_eq!(command, Command::Help);
+    }
+
+    #[test]
+    fn test_arguments_with_version() {
+        let arguments = vec![
+            "gitql".to_string(),
+            "dummy".to_string(),
+            "--version".to_string(),
+        ];
+        let command = parse_arguments(&arguments);
+        assert_eq!(command, Command::Version);
+    }
+
+    #[test]
+    fn test_arguments_with_valid_page_size() {
+        let arguments = vec![
+            "gitql".to_string(),
+            "--pagesize".to_string(),
+            "10".to_string(),
+        ];
+        let command = parse_arguments(&arguments);
+        assert!(!matches!(command, Command::Error { .. }));
+    }
+
+    #[test]
+    fn test_arguments_with_invalid_page_size() {
+        let arguments = vec![
+            "gitql".to_string(),
+            "--pagesize".to_string(),
+            "-".to_string(),
+        ];
+        let command = parse_arguments(&arguments);
+        assert!(matches!(command, Command::Error { .. }));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use gitql_cli::arguments;
+use gitql_cli::arguments::Arguments;
+use gitql_cli::arguments::Command;
 use gitql_cli::render;
 use gitql_cli::reporter;
+use gitql_cli::reporter::DiagnosticReporter;
 use gitql_engine::engine;
 use gitql_parser::parser;
 use gitql_parser::tokenizer;
@@ -8,25 +11,53 @@ use gitql_parser::tokenizer;
 fn main() {
     if cfg!(debug_assertions) {
         std::env::set_var("RUST_BACKTRACE", "1");
+        std::env::set_var("RUST_LIB_BACKTRACE", "1");
     }
 
-    let arguments = arguments::parse_arguments();
-    let mut reporter = reporter::DiagnosticReporter::default();
-    let mut git_repositories: Vec<gix::Repository> = vec![];
-    for repsitory in arguments.repos {
-        let git_repository = gix::open(repsitory);
-        if git_repository.is_err() {
-            reporter.report_error(&git_repository.err().unwrap().to_string());
-            return;
+    let args = std::env::args().collect();
+    let command = arguments::parse_arguments(&args);
+
+    match command {
+        Command::ReplMode(arguments) => {
+            launch_gitql_repl(arguments);
         }
-        git_repositories.push(git_repository.ok().unwrap());
+        Command::QueryMode(query, arguments) => {
+            let mut reporter = reporter::DiagnosticReporter::default();
+            let git_repos_result = validate_git_repositories(&arguments.repos);
+            if git_repos_result.is_err() {
+                reporter.report_error(git_repos_result.err().unwrap().as_str());
+                return;
+            }
+
+            let repos = git_repos_result.ok().unwrap();
+            execute_gitql_query(query, &arguments, &repos, &mut reporter);
+        }
+        Command::Help => {
+            arguments::print_help_list();
+        }
+        Command::Version => {
+            let version = env!("CARGO_PKG_VERSION");
+            println!("GitQL version {}", version);
+        }
+        Command::Error(error_mssage) => {
+            println!("{}", error_mssage);
+        }
     }
+}
+
+fn launch_gitql_repl(arguments: Arguments) {
+    let mut reporter = reporter::DiagnosticReporter::default();
+    let git_repos_result = validate_git_repositories(&arguments.repos);
+    if git_repos_result.is_err() {
+        reporter.report_error(git_repos_result.err().unwrap().as_str());
+        return;
+    }
+
+    let git_repositories = git_repos_result.ok().unwrap();
 
     let mut input = String::new();
-
     loop {
         print!("gql > ");
-
         std::io::Write::flush(&mut std::io::stdout()).expect("flush failed!");
 
         match std::io::stdin().read_line(&mut input) {
@@ -44,53 +75,77 @@ fn main() {
             break;
         }
 
-        let front_start = std::time::Instant::now();
-        let tokenizer_result = tokenizer::tokenize(trimed_input.to_string());
-        if tokenizer_result.is_err() {
-            reporter.report_gql_error(tokenizer_result.err().unwrap());
-            input.clear();
-            continue;
-        }
-
-        let tokens = tokenizer_result.ok().unwrap();
-        let parser_result = parser::parse_gql(tokens);
-        if parser_result.is_err() {
-            reporter.report_gql_error(parser_result.err().unwrap());
-            input.clear();
-            continue;
-        }
-
-        let statements = parser_result.ok().unwrap();
-        let front_duration = front_start.elapsed();
-
-        let engine_start = std::time::Instant::now();
-        let evaluation_result = engine::evaluate(&git_repositories, statements);
-
-        // Report Runtime exceptions if they exists
-        if evaluation_result.is_err() {
-            reporter.report_runtime_error(evaluation_result.err().unwrap());
-            input.clear();
-            continue;
-        }
-
-        let mut evaluation_values = evaluation_result.ok().unwrap();
-        render::render_objects(
-            &mut evaluation_values.groups,
-            &evaluation_values.hidden_selections,
-            arguments.pagination,
-            arguments.size_per_page,
+        execute_gitql_query(
+            trimed_input.to_owned(),
+            &arguments,
+            &git_repositories,
+            &mut reporter,
         );
 
-        let engine_duration = engine_start.elapsed();
         input.clear();
-
-        if arguments.analysis {
-            println!("\n");
-            println!("Analysis:");
-            println!("Frontend : {:?}", front_duration);
-            println!("Engine   : {:?}", engine_duration);
-            println!("Total    : {:?}", (front_duration + engine_duration));
-            println!("\n");
-        }
     }
+}
+
+fn execute_gitql_query(
+    query: String,
+    arguments: &Arguments,
+    repos: &[gix::Repository],
+    reporter: &mut DiagnosticReporter,
+) {
+    let front_start = std::time::Instant::now();
+    let tokenizer_result = tokenizer::tokenize(query);
+    if tokenizer_result.is_err() {
+        reporter.report_gql_error(tokenizer_result.err().unwrap());
+        return;
+    }
+
+    let tokens = tokenizer_result.ok().unwrap();
+    let parser_result = parser::parse_gql(tokens);
+    if parser_result.is_err() {
+        reporter.report_gql_error(parser_result.err().unwrap());
+        return;
+    }
+
+    let statements = parser_result.ok().unwrap();
+    let front_duration = front_start.elapsed();
+
+    let engine_start = std::time::Instant::now();
+    let evaluation_result = engine::evaluate(repos, statements);
+
+    // Report Runtime exceptions if they exists
+    if evaluation_result.is_err() {
+        reporter.report_runtime_error(evaluation_result.err().unwrap());
+        return;
+    }
+
+    let mut evaluation_values = evaluation_result.ok().unwrap();
+    render::render_objects(
+        &mut evaluation_values.groups,
+        &evaluation_values.hidden_selections,
+        arguments.pagination,
+        arguments.page_size,
+    );
+
+    let engine_duration = engine_start.elapsed();
+
+    if arguments.analysis {
+        println!("\n");
+        println!("Analysis:");
+        println!("Frontend : {:?}", front_duration);
+        println!("Engine   : {:?}", engine_duration);
+        println!("Total    : {:?}", (front_duration + engine_duration));
+        println!("\n");
+    }
+}
+
+fn validate_git_repositories(repositories: &Vec<String>) -> Result<Vec<gix::Repository>, String> {
+    let mut git_repositories: Vec<gix::Repository> = vec![];
+    for repsitory in repositories {
+        let git_repository = gix::open(repsitory);
+        if git_repository.is_err() {
+            return Err(git_repository.err().unwrap().to_string());
+        }
+        git_repositories.push(git_repository.ok().unwrap());
+    }
+    Ok(git_repositories)
 }
