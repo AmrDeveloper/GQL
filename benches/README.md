44a17935783702baead049f8e96130c04bc49f54
AmrDeveloper <amr96@programmer.net>
2024-06-27 23:07:11 +0200

    Setup benchmark code for the tokenizer

---
+++ benches/README.md
@@ -0,0 +1,18 @@
+<h1 align="center">GQL - Git Query Language</h1></br>
+
+<p align="center">
+<img src="../media/gql_logo.svg" width="20%" height="20%"/>
+</p>
+
+<p align="center">
+  <img alt="Crates.io" src="https://img.shields.io/crates/v/gitql?style=flat-square">
+  <img alt="Deps" src="https://deps.rs/repo/github/amrdeveloper/gql/status.svg">
+  <img alt="Release" src="https://github.com/AmrDeveloper/GQL/actions/workflows/release.yaml/badge.svg">
+  <img alt="Docs" src="https://github.com/AmrDeveloper/GQL/actions/workflows/docs.yaml/badge.svg">
+  <img alt="GitHub release" src="https://img.shields.io/github/v/release/amrdeveloper/gql">
+  <img alt="GitHub issues" src="https://img.shields.io/github/issues/amrdeveloper/gql">
+  <img alt="GitHub" src="https://img.shields.io/github/license/amrdeveloper/gql">
+  <img alt="GitHub all releases" src="https://img.shields.io/github/downloads/amrdeveloper/gql/total">
+</p>
+
+Run all benchmarks with `cargo bench`
---
+++ benches/benchmarks.rs
@@ -0,0 +1,47 @@
+use criterion::black_box;
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::Criterion;
+use gitql_parser::tokenizer::tokenize;
+
+const QUERY_100_CHAR: &str = "SELECT name, COUNT(name) FROM commits GROUP BY name, author_email ORDER BY commit_num DESC LIMIT 100";
+
+fn tokenizer_100_char_benchmark(c: &mut Criterion) {
+    c.bench_function("Tokenizer 100 Char", |b| {
+        b.iter(|| tokenize(black_box(QUERY_100_CHAR.to_owned())))
+    });
+}
+
+fn tokenizer_100k_char_benchmark(c: &mut Criterion) {
+    let query_100k_char = QUERY_100_CHAR.repeat(100_000 / 100);
+    c.bench_function("Tokenizer 100K Char", |b| {
+        b.iter(|| tokenize(black_box(query_100k_char.to_owned())))
+    });
+}
+
+fn tokenizer_1m_char_benchmark(c: &mut Criterion) {
+    let query_100k_char = QUERY_100_CHAR.repeat(1_000_000 / 100);
+    c.bench_function("Tokenizer 1M Char", |b| {
+        b.iter(|| tokenize(black_box(query_100k_char.to_owned())))
+    });
+}
+
+fn tokenizer_10m_char_benchmark(c: &mut Criterion) {
+    let query_100k_char = QUERY_100_CHAR.repeat(10_000_000 / 100);
+    c.bench_function("Tokenizer 10M Char", |b| {
+        b.iter(|| tokenize(black_box(query_100k_char.to_owned())))
+    });
+}
+
+criterion_group! {
+   name = benches;
+   config = Criterion::default().significance_level(0.1).sample_size(10);
+   targets =
+   // Tokenizer
+   tokenizer_100_char_benchmark,
+   tokenizer_100k_char_benchmark,
+   tokenizer_1m_char_benchmark,
+   tokenizer_10m_char_benchmark
+}
+
+criterion_main!(benches);
