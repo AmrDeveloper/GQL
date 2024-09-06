ca158de3bb2154b3070f8292604dbabc3bc795ef
AmrDeveloper <amr96@programmer.net>
2024-09-06 21:24:50 +0200

    Register range functions and signatures to the std

--- crates/gitql-std/src/function.rs
+++ crates/gitql-std/src/function.rs
@@ -2,6 +2,7 @@ use crate::array::*;
 use crate::datetime::*;
 use crate::general::*;
 use crate::number::*;
+use crate::range::*;
 use crate::regex::*;
 use crate::text::*;
 
@@ -20,6 +21,7 @@ pub fn standard_functions() -> &'static HashMap<&'static str, Function> {
         register_std_general_functions(&mut map);
         register_std_regex_functions(&mut map);
         register_std_array_functions(&mut map);
+        register_std_range_functions(&mut map);
         map
     })
 }
@@ -34,6 +36,7 @@ pub fn standard_function_signatures() -> &'static HashMap<&'static str, Signatur
         register_std_general_function_signatures(&mut map);
         register_std_regex_function_signatures(&mut map);
         register_std_array_function_signatures(&mut map);
+        register_std_range_function_signatures(&mut map);
         map
     })
 }
