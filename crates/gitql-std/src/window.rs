d783af20f2a9ca7a37574cb116d6e0d1289817a2
AmrDeveloper <amr96@programmer.net>
2024-12-15 11:53:56 +0100

    feat: Implement `ROW_NUMBER` window function

--- crates/gitql-std/src/window.rs
+++ crates/gitql-std/src/window.rs
@@ -7,6 +7,7 @@ use gitql_ast::types::integer::IntType;
 use gitql_core::signature::Signature;
 use gitql_core::signature::WindowFunction;
 use gitql_core::values::base::Value;
+use gitql_core::values::integer::IntValue;
 use gitql_core::values::null::NullValue;
 
 use crate::meta_types::first_element_type;
@@ -18,6 +19,7 @@ pub fn window_functions() -> &'static HashMap<&'static str, WindowFunction> {
         map.insert("first_value", window_first_value);
         map.insert("nth_value", window_nth_value);
         map.insert("last_value", window_last_value);
+        map.insert("row_number", window_row_number);
         map
     })
 }
@@ -51,6 +53,14 @@ pub fn window_function_signatures() -> HashMap<&'static str, Signature> {
             return_type: Box::new(DynamicType {
                 function: first_element_type,
             }),
+        },
+    );
+
+    map.insert(
+        "row_number",
+        Signature {
+            parameters: vec![],
+            return_type: Box::new(IntType),
         },
     );
     map
@@ -88,6 +98,16 @@ pub fn window_last_value(frame: &[Vec<Box<dyn Value>>]) -> Vec<Box<dyn Value>> {
     let mut values = Vec::with_capacity(frame_len);
     for _ in 0..frame_len {
         values.push(last_value.clone());
+    }
+    values
+}
+
+pub fn window_row_number(frame: &[Vec<Box<dyn Value>>]) -> Vec<Box<dyn Value>> {
+    let frame_len = frame.len();
+    let mut values: Vec<Box<dyn Value>> = Vec::with_capacity(frame_len);
+    for i in 0..frame_len {
+        let num = i as i64 + 1;
+        values.push(Box::new(IntValue { value: num }));
     }
     values
 }
--- docs/functions/window.md
+++ docs/functions/window.md
@@ -1,7 +1,8 @@
 A Window function in GitQL performs a calculation on a window (frame) of values and returns a single value
 
-| Name        | Parameters | Return | Description                                |
-| ----------- | ---------- | ------ | ------------------------------------------ |
-| FIRST_VALUE | ANY        | Any    | Return first value in the window of values |
-| NTH_VALUE   | ANY, INT   | Any    | Return n value in the window of values     |
-| LAST_VALUE  | ANY        | Any    | Return last value in the window of values  |
+| Name        | Parameters | Return | Description                                                                        |
+| ----------- | ---------- | ------ | ---------------------------------------------------------------------------------- |
+| FIRST_VALUE | ANY        | Any    | Return first value in the window of values                                         |
+| NTH_VALUE   | ANY, INT   | Any    | Return n value in the window of values                                             |
+| LAST_VALUE  | ANY        | Any    | Return last value in the window of values                                          |
+| ROW_NUMBER  |            | INT    | Return unique sequential integer to each row within the partition, starting from 1 |
