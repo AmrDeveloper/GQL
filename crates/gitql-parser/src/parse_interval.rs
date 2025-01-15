f6505a73e5cf08b7288e57b76f5ccb61127a4f19
AmrDeveloper <amr96@programmer.net>
2025-01-15 19:07:01 +0100

    Check that interval value in range -170M to 170M

--- crates/gitql-parser/src/parse_interval.rs
+++ crates/gitql-parser/src/parse_interval.rs
@@ -9,6 +9,8 @@ use crate::token::SourceLocation;
 use crate::token::Token;
 use crate::token::TokenKind;
 
+const INTERVAL_MAX_VALUE: i64 = 170_000_000;
+
 pub(crate) fn parse_interval_expression(
     tokens: &[Token],
     position: &mut usize,
@@ -48,6 +50,14 @@ fn parse_interval_literal(
     let mut position = 0;
     let mut interval = Interval::default();
     let tokens = interval_str.split_whitespace().collect::<Vec<&str>>();
+
+    let mut has_years = false;
+    let mut has_months = false;
+    let mut has_days = false;
+    let mut has_hours = false;
+    let mut has_minutes = false;
+    let mut has_seconds = false;
+
     while position < tokens.len() {
         let token = tokens[position].trim();
         if token.is_empty() {
@@ -56,8 +66,10 @@ fn parse_interval_literal(
         }
 
         // Parse Days, Months or Years
-        if let Ok(value) = token.parse::<i32>() {
+        if let Ok(value) = token.parse::<i64>() {
+            // Consume value
             position += 1;
+
             if position >= tokens.len() {
                 return Err(Diagnostic::error(&format!(
                     "Missing interval unit after value {}",
@@ -70,18 +82,21 @@ fn parse_interval_literal(
             // Parse the unit
             let maybe_unit = tokens[position];
             if matches!(maybe_unit, "year" | "years") {
+                check_interval_value_and_unit(&mut has_years, value, maybe_unit, location)?;
                 interval.years = value;
                 position += 1;
                 continue;
             }
 
             if matches!(maybe_unit, "mon" | "mons") {
+                check_interval_value_and_unit(&mut has_months, value, maybe_unit, location)?;
                 interval.months = value;
                 position += 1;
                 continue;
             }
 
             if matches!(maybe_unit, "day" | "days") {
+                check_interval_value_and_unit(&mut has_days, value, maybe_unit, location)?;
                 interval.days = value;
                 position += 1;
                 continue;
@@ -104,8 +119,9 @@ fn parse_interval_literal(
                     .as_boxed());
             }
 
-            match time_parts[0].parse::<i32>() {
+            match time_parts[0].parse::<i64>() {
                 Ok(hours) => {
+                    check_interval_value_and_unit(&mut has_hours, hours, time_parts[0], location)?;
                     interval.hours = hours;
                 }
                 Err(_) => {
@@ -115,9 +131,15 @@ fn parse_interval_literal(
                 }
             }
 
-            match time_parts[1].parse::<i32>() {
-                Ok(hours) => {
-                    interval.minutes = hours;
+            match time_parts[1].parse::<i64>() {
+                Ok(minutes) => {
+                    check_interval_value_and_unit(
+                        &mut has_minutes,
+                        minutes,
+                        time_parts[1],
+                        location,
+                    )?;
+                    interval.minutes = minutes;
                 }
                 Err(_) => {
                     return Err(Diagnostic::error("Invalid input syntax for type interval")
@@ -129,6 +151,12 @@ fn parse_interval_literal(
             if time_parts.len() == 3 {
                 match time_parts[2].parse::<f64>() {
                     Ok(seconds) => {
+                        check_interval_value_and_unit(
+                            &mut has_seconds,
+                            seconds as i64,
+                            time_parts[2],
+                            location,
+                        )?;
                         interval.seconds = seconds;
                     }
                     Err(_) => {
@@ -139,6 +167,7 @@ fn parse_interval_literal(
                 }
             }
 
+            // Consume time token
             position += 1;
             continue;
         }
@@ -150,4 +179,33 @@ fn parse_interval_literal(
     }
 
     Ok(interval)
+}
+
+fn check_interval_value_and_unit(
+    is_used_twice: &mut bool,
+    interval_value: i64,
+    unit_name: &str,
+    location: SourceLocation,
+) -> Result<(), Box<Diagnostic>> {
+    if !*is_used_twice {
+        *is_used_twice = true;
+        return Ok(());
+    }
+
+    if !(-INTERVAL_MAX_VALUE..=INTERVAL_MAX_VALUE).contains(&interval_value) {
+        return Err(Diagnostic::error(&format!(
+            "Interval value for unit `{}` is out of the range",
+            unit_name
+        ))
+        .add_help("Interval value must be in range from -170_000_000 to 170_000_000")
+        .with_location(location)
+        .as_boxed());
+    }
+
+    Err(Diagnostic::error(&format!(
+        "Can't use the same interval unit `{}` twice",
+        unit_name
+    ))
+    .with_location(location)
+    .as_boxed())
 }
