7288cace90d227902e4c0a731db67090f5164548
AmrDeveloper <amr96@programmer.net>
2025-10-05 15:13:16 +0200

    Fix docs-tests on CI

--- crates/gitql-core/src/schema.rs
+++ crates/gitql-core/src/schema.rs
@@ -2,32 +2,7 @@ use std::collections::HashMap;
 
 use gitql_ast::types::DataType;
 
-/// A Representation of the Data Schema that constructed the following
-///
-/// [`tables_fields_names`]  is a map of tables and columns names
-///
-/// # Examples
-///
-/// ```
-///
-/// pub static ref TABLES_FIELDS_NAMES: HashMap<&'static str, Vec<&'static str>> = {
-///    let mut map = HashMap::new();
-///    map.insert("refs", vec!["name", "full_name", "type", "repo"]);
-/// }
-///
-/// ```
-///
-/// [`tables_fields_types`] is a map of each column name in general with the expected data type
-///
-/// # Examples
-///
-/// ```
-/// pub static ref TABLES_FIELDS_TYPES: HashMap<&'static str, Box<dyn DataType>> = {
-///    let mut map = HashMap::new();
-///    map.insert("commit_id", Box::new(TextType));
-/// }
-/// ```
-///
+/// A Representation of the Schema of the data including columns, tables and types
 pub struct Schema {
     pub tables_fields_names: HashMap<&'static str, Vec<&'static str>>,
     pub tables_fields_types: HashMap<&'static str, Box<dyn DataType>>,
